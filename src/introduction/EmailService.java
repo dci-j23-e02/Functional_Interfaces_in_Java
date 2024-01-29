23eb55ec0b44579fab788e7e04d18097804fd03a
safwan-kher <safwan.kherallah@digitalcareerinstitute.org>
2024-01-29 10:41:37 +0100

    init commit

--- src/introduction/EmailService.java
+++ src/introduction/EmailService.java
@@ -1,5 +1,12 @@
 package introduction;
 
 public class EmailService {
-// In a real application,
+// In a real application, we would have code here to send emails.
+  // This is just a simulation
+
+  public void sendEmail(String to, String message){
+    System.out.println("Sending email to: "+ to);
+    System.out.println(message);
+    System.out.println("Email sent successfully!\n");
+  }
 }
--- src/introduction/FunctionExmple.java
+++ src/introduction/FunctionExmple.java
@@ -1,7 +1,10 @@
 package introduction;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 import java.util.function.Function;
+import java.util.stream.Collectors;
 
 public class FunctionExmple {
 
@@ -13,6 +16,43 @@ public class FunctionExmple {
       new User("Charlie", "charlie@example.com")
     );
 
-    Function<String,String> printNames ;
+
+    // A function to extract emails
+
+    // Function<User,String> extractEmail = User :: getEmail;
+    Function<User,String> extractEmail = user -> user.getEmail();
+
+
+    // Example of using Function in Java:
+    Map<String, Integer> nameMap = new HashMap<>();
+
+          // In the below line the Function is used :
+          // Integer value = nameMap.computeIfAbsent("Safwan", s -> s.length());
+          Integer value = nameMap.computeIfAbsent("Safwan", String :: length);
+
+          // System.out.println(value);
+    // Function to create invitation message
+    Function<User, String> createInvitation = user -> {
+      String userName = user.getName();
+      String message = "Dear "+userName+",\n You atre invited to our conference.";
+      return message;
+    };
+
+    // Instance of EmailService
+    EmailService es = new EmailService();
+
+    // Combine the functions to send out invitations
+    users.stream()
+        .map(
+            user -> {
+              String email  = extractEmail.apply(user);
+              String invitation = createInvitation.apply(user);
+              es.sendEmail(email, invitation); // Use Email Service to send the email
+              return  email;
+            }
+        )
+        .collect(Collectors.toList());
+
+    System.out.println("Invitations sent to all users.");
   }
 }
