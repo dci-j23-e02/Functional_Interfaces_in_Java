6986195b2bd5cedef77f1a181397714613aee568
safwan-kher <safwan.kherallah@digitalcareerinstitute.org>
2024-01-30 10:30:35 +0100

    init commit

--- src/consumers/CheckoutService.java
+++ src/consumers/CheckoutService.java
@@ -1,5 +1,24 @@
 package consumers;
 
-public class CheckoutService {
+import java.util.function.BiConsumer;
+import java.util.function.Consumer;
 
+public class CheckoutService {
+  private Consumer<Order> processOrder;
+  private BiConsumer<Order,Double> applyDiscount;
+
+  public CheckoutService(Consumer<Order> processOrder,
+      BiConsumer<Order, Double> applyDiscount) {
+    this.processOrder = processOrder;
+    this.applyDiscount = applyDiscount;
+  }
+
+
+  public void checkout(Order o, double discount){
+    if(discount > 0){
+      applyDiscount.accept(o, discount);
+    }
+
+    processOrder.accept(o);
+  }
 }
--- src/consumers/ConsumerExample.java
+++ src/consumers/ConsumerExample.java
@@ -1,8 +1,26 @@
 package consumers;
 
+import java.util.Scanner;
+import java.util.function.BiConsumer;
+import java.util.function.Consumer;
+
 public class ConsumerExample {
 
   public static void main(String[] args) {
+    Scanner input = new Scanner(System.in);
+    Order order = new Order();
+
+    Consumer<Order> processOrder = o -> System.out.println(
+        "Order processed. Total: $" +
+        String.format(
+            "%.2f", o.getTotalPrice()
+        )
+    );
+
+    BiConsumer<Order,Double> applyDiscount = (o,d) -> {
+      o.applyDiscount(d);
+      System.out.println("Discount of $" + d + " applied. New total: $" + String.format( "%.2f", o.getTotalPrice()));
+    };
 
   }
 }
